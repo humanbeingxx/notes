d64d412e6b398251d229836427c9656f275a2984
xiaoshuang.cui <humanbeingxx@sina.com>
2018-10-08 21:08:28 +0800

    add new file

--- daily/makefile.py
+++ daily/makefile.py
@@ -4,5 +4,5 @@ import os,shutil
 
 cur_path = os.getcwd()
 
-for i in range(1,31):
-    shutil.copy("daily/template.md", "daily/2018-09-%02d.md"% i)
+for i in range(1,32):
+    shutil.copy("daily/template.md", "daily/2018-10-%02d.md"% i)
